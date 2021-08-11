bf9c96889da1dce30b8e89020fba192297536ded
VertOlive <vertolive13@gmail.com>
2021-08-11 08:46:35 +0200

    Update Event manager.

--- event.h
+++ event.h
@@ -1,6 +1,7 @@
 #ifndef EVENT_H
 #define EVENT_H
 
+#include <functional>
 #include <memory>
 #include <map>
 
@@ -9,34 +10,40 @@ class Event;
 
 /* All call back must have the same template type */
 template <typename T, typename U>
-class Callback {
+class Next {
 public:
-  U operator()(T args) {
-    return *to_event(args);
+  Next(auto event_ptr) : to_event(event), decorator([](T args) -> T {return args;}) {}
+  Next(auto event_ptr, std::function<T(T)> dec) : to_event(event_ptr), decorator(dec) {}
+
+  U operator()(T args, std::function<void(T)> callback) {
+    callback(args);
+    return *to_event(decorator(args));
   };
-protected:
+private:
   std::shared_ptr<Event<T, U> > to_event;
+  std::function<T(T)> decorator;
 };
 
+template <typename T, typename U>
+class Logic {virtual U operator()(T args) = 0;};
 
 template <typename T, typename U>
 class Logic_event {
+public:
+  Logic_event(std::map<std::string, Next<T,U>> &&next_map) {
+    to_next = std::move(next_map);
+  }
+  virtual U operator()(T args) = 0;
 protected:
-  U call(std::string callback_id, T args) {
-    return to_callbacks[callback_id](args);
+  U call(int next_event_id, T args, std::function<void(T)> callback) {
+    return to_next[next_event_id](args, callback(args));
   }
 
-  void add_callback(std::string id, Callback<T, U> callback){
-    to_callbacks.insert(id, callback);
+  void add_next(Next<T, U> next){
+    to_next.push_back(next);
   }
-
-  std::map<std::string ,Callback<T, U> > to_callbacks;
-};
-
-template<typename T, typename U>
-struct Event {
-  Logic_event<T,U> logic;
-  Callback<T,U> callback;
+private:
+  std::vector<std::string ,Next<T, U> > to_next;
 };
 
 #endif /* event.h */
